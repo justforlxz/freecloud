25315ae5d4159a5b501d12c12b678c22f47207fd
kirigaya kazushin <zhuzi@linuser.org>
2016-06-28 20:41:44 +0800

    更新unity脚本

---
+++ Move.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	public float jumpSpeed = 8.0f;
+	public float gravity = 20.0f;
+	public float speed = 0.5f;
+	private CharacterController controller;
+	public Animator anim;
+	private Vector3 moveDirection = Vector3.zero;
+	private float inputH;
+	private float inputV;
+
+	public Rigidbody rbody;
+	private bool run;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		run = false;
+		rbody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		CharacterController controller = GetComponent<CharacterController>();
+		if (controller.isGrounded) {
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= speed;
+//			if (Input.GetButton("Jump"))
+//				moveDirection.y = jumpSpeed;
+		}
+		speed = inputV * 50f * Time.deltaTime;
+		if (run) {
+			speed = 3f;
+		}
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
+
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			run = true;
+		} else {
+			run = false;
+		}
+		if (Input.GetKey (KeyCode.Space)) {
+			anim.SetBool ("jump", true);
+		} else {
+			anim.SetBool ("jump", false);
+		
+		}
+
+		inputH = Input.GetAxis ("Horizontal");
+		inputV = Input.GetAxis ("Vertical");
+		anim.SetBool ("run",run);
+
+		anim.SetFloat ("inputH",inputH);
+		anim.SetFloat ("inputV",inputV);
+
+//		float moveX = inputH * 20f * Time.deltaTime;
+//		float moveZ = inputV * 50f * Time.deltaTime;
+//		if (moveZ<=0f) {
+//			moveX = 0f;
+//		}
+//		rbody.velocity=new Vector3(moveX,0f,moveZ);
+	}
+}
---
+++ mouselook1.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mouselook1 : MonoBehaviour {
+
+	public float moveSpeed = 2f;
+	public float rotateSpeed = 2f;
+
+	public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+	public RotationAxes axes = RotationAxes.MouseXAndY;
+	public float sensitivityX = 15F;
+	public float sensitivityY = 15F;
+
+	public float minimumX = -360F;
+	public float maximumX = 360F;
+
+	public float minimumY = -85F;
+	public float maximumY = 10F;
+
+	public float rotationY = 0F;
+	public float theDistance = -3.34f;
+	public float MaxDistance = -10f;
+	public GameObject target;
+
+
+
+	public float minFov = 40f;
+	public float maxFov = 90f;
+	public float sensitivity = 15f;
+
+	void Update ()
+	{
+
+		target = GameObject.Find("unitychan");
+		if(theDistance>0)
+			theDistance = 0;
+		if(theDistance < MaxDistance)
+			theDistance = MaxDistance;
+		//if(Input.GetMouseButton(1))
+		//{
+
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+		if (h != 0 || v != 0)
+		{
+			Vector3 targetDirection = new Vector3(h, 0, v);
+			float y = Camera.main.transform.rotation.eulerAngles.y;
+			targetDirection = Quaternion.Euler(0, y, 0) * targetDirection;
+
+			transform.Translate(targetDirection * Time.deltaTime * moveSpeed,Space.World);
+		}
+		float fov = Camera.main.fieldOfView;
+		fov += Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+		fov = Mathf.Clamp(fov, minFov, maxFov);
+		Camera.main.fieldOfView = fov;
+//		if (Input.GetKey(KeyCode.J))
+//		{
+//			transform.Rotate(-Vector3.up * Time.deltaTime * rotateSpeed);
+//
+//		}
+			transform.position = target.transform.position;
+			if (axes == RotationAxes.MouseXAndY)
+			{
+				float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+				rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+				if (rotationY>=-4) {
+					rotationY = -4;
+				}else if (rotationY>=80) {
+					rotationY = 80;
+				}
+				rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+				transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+				target.transform.localEulerAngles=new Vector3(0, rotationX, 0);
+			}
+			else if (axes == RotationAxes.MouseX)
+			{
+				transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+			}
+			else
+			{
+				rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+				rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+				transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+			}
+			SetDistance();
+		    
+		//}
+		//else
+		//{
+		//	transform.position = target.transform.position;
+		//	SetDistance();
+	//	}
+	}
+
+	void Start ()
+	{
+		if (GetComponent<Rigidbody>())
+		{
+			GetComponent<Rigidbody>().freezeRotation = true;
+			transform.position = target.transform.position;
+		}
+	}
+
+	//设置相机与人物之间的距离
+	void SetDistance()
+	{
+		Cursor.visible = false;
+		transform.Translate(Vector3.forward * theDistance);
+	}
+}
